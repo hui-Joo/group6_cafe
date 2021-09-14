4a70b6ef5981a87a670ac3bc85b75646a266f586
Choi.SY <sollly0829@gmail.com>
2021-09-14 13:38:16 +0900

    group6_cafe/manager/commonMA/ Admin_DTO -> ManagerDTO, 변수 소문자 변경

--- src/manager/chart/ChartMain.java
+++ src/manager/chart/ChartMain.java
@@ -1,5 +1,18 @@
 package manager.chart;
 
-public class ChartMain {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class ChartMain extends Application {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 }
