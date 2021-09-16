56b486b34be9d2ab15198f9808b3054d2c944098
hjoo <oct20ray@gmail.com>
2021-09-16 09:18:20 +0900

    버튼 동작

---
+++ src/oder/Inquiry.java
@@ -0,0 +1,41 @@
+package oder;
+
+public class Inquiry {
+	private String Americano;
+	private String Latte;
+	private String Cappuccino;
+	private String VanillaLatte;
+	private int price;
+	
+	
+	public String getAmericano() {
+		return Americano;
+	}
+	public void setAmericano(String americano) {
+		Americano = americano;
+	}
+	public String getLatte() {
+		return Latte;
+	}
+	public void setLatte(String latte) {
+		Latte = latte;
+	}
+	public String getCappuccino() {
+		return Cappuccino;
+	}
+	public void setCappuccino(String cappuccino) {
+		Cappuccino = cappuccino;
+	}
+	public String getVanillaLatte() {
+		return VanillaLatte;
+	}
+	public void setVanillaLatte(String vanillaLatte) {
+		VanillaLatte = vanillaLatte;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
