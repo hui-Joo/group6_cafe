105756a610c06f3569d1412da9d1580cdf280cee
JCH <flfk6354@naver.com>
2021-09-17 19:22:42 +0900

    manager.admin 수정

--- src/manager/admin/Admin_Controller.java
+++ src/manager/admin/Admin_Controller.java
@@ -8,25 +8,30 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-<<<<<<< HEAD
+
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
+/*
 public class Admin_Controller implements Initializable {
 	private Parent root;
 	private Object txtPassword;
-=======
+*/	
+	
+
 import javafx.stage.Stage;
 import manager.chart.Chart_Main;
 import manager.stock.StockMain;
 
 public class Admin_Controller implements Initializable {
+	
+//}
 	private Parent root;
+	private Object txtPassword;
 	StockMain sm ;
 	Stage stage;
 	Chart_Main cm;
->>>>>>> ea8b8e1d7678ff52f2f5c4bb77d2e347aaee65c6
+
 
 	public void setRoot(Parent root) {
 		this.root = root;
