13b49320d16001d8d8c672efec5de5dd6a05cb44
JCH <flfk6354@naver.com>
2021-09-23 08:41:06 +0900

    씬빌더

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,6 +1,26 @@
 package main;
 
-public class MainClass {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class MainClass extends Application{
+	
+	public void start(Stage primaryStage) throws Exception{
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("coffee_loading.fxml"));
+		Parent root = loader.load();
+		
+		Controller con = loader.getController();
+		con.setRoot(root);
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
 ////
