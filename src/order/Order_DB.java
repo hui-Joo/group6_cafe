251c66f8ce08a2aeb4be5d49b23600f709f7fb27
hjoo <oct20ray@gmail.com>
2021-09-24 20:22:13 +0900

    orderDB

--- src/order/Order_DB.java
+++ src/order/Order_DB.java
@@ -2,21 +2,28 @@ package order;
 
 import java.sql.PreparedStatement;
 
+
+import common.commonDB.DBClass;
 import manager.commonMA.ManagerDTO;
 
 public class Order_DB {
 	OrderDTO od = new OrderDTO();
-	public int update(ManagerDTO dto){
+	public int updateMenu(ManagerDTO managerDto, OrderDTO orderDto){
 		
 		String sql = "update \"MENUDB\" set americano=?, latte=?, capucino=?, vanillaLatte=? where num=1";
 		int result = 0;
 		 
 		try {
 			PreparedStatement ps = common.commonDB.DBClass.conn.prepareStatement(sql);
-			ps.setInt(1, od.getAmericano());
-			ps.setInt(2, od.getLatte());
-			ps.setInt(3, od.getCapuccino());
-			ps.setInt(4, od.getVanilaLatte());
+			ps.setInt(1, orderDto.getAmericano());
+			ps.setInt(2, orderDto.getLatte());
+			ps.setInt(3, orderDto.getCapuccino());
+			ps.setInt(4, orderDto.getVanilaLatte());
+			
+			ps.setInt(1, orderDto.getCntA());
+			ps.setInt(2, orderDto.getCntL());
+			ps.setInt(3, orderDto.getCntC());
+			ps.setInt(4, orderDto.getCntV());
 			
 			result = ps.executeUpdate();
 		}catch (Exception e) {
